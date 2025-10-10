5e54c9753e6c7614375551bb7e65b4787dc5d2b6
Juan Butron <yosoyunclap@gmail.com>
2025-10-10 11:49:13 -0400

    Correccion del modal AgendarCita y añadir el componente AgendarCitaModal + el page

--- src/app/epic_example/components/AgendarCitaButton.tsx
+++ src/app/epic_example/components/AgendarCitaButton.tsx
@@ -1,5 +1,7 @@
+//Componente del botón agendar cita + estado de abrir/cerrar el modal
 "use client";
 import { useState } from "react";
+import AgendarCitaModal from "../components/AgendarCitaModal";
 
 export default function AppointmentButton() {
   const [open, setOpen] = useState(false);
@@ -8,26 +10,13 @@ export default function AppointmentButton() {
     <>
       <button
         onClick={() => setOpen(true)}
-        className="px-4 py-2 bg-[#4289CC] text-white rounded hover:bg-blue-500"
+        className="px-5 py-2 bg-[#4289CC] text-white rounded-lg hover:bg-blue-700 transition transform hover:scale-105 duration-300 font-medium"
       >
-        Agendar Cita
+        + Appointment
       </button>
 
-      {open && (
-        <div className="fixed inset-0 flex items-center justify-center">
-          <div className="relative bg-white p-6 rounded shadow-lg w-[400px]">
-            <button
-              onClick={() => setOpen(false)}
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 text-xl font-bold"
-            >
-              ×
-            </button>
-
-            <h1 className="text-4xl font-bold mb-6">Agendar cita</h1>
-            <p>...Aquí irá el calendario con horarios...</p>
-          </div>
-        </div>
-      )}
+      {/* 🪟 Modal */}
+      {open && <AgendarCitaModal onClose={() => setOpen(false)} />}
     </>
   );
 }
--- src/app/epic_example/page.tsx
+++ src/app/epic_example/page.tsx
@@ -6,9 +6,20 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
       <h1 className="text-4xl font-bold mb-6">Entorno de prueba para ver el componente de Agendar Cita :O </h1>
-      <p className="text-lg mb-4">This is the home screen. Feel free to explore!</p>
-      <div className="space-x-4">
-        <AppointmentButton /> 
+      
+      <div className="relative w-full max-w-lg p-6 bg-white rounded-lg shadow-md">
+        <div>
+          <h3 className="text-xl font-semibold text-gray-900">Consultoría en Marketing Digital</h3>
+          <p className="mt-2 text-gray-600">
+            María Rodríguez - Especialista en Marketing Digital <br />
+            <br />Sesión de consultoría personalizada para optimizar tu estrategia digital. Incluye análisis de redes sociales, SEO y campañas publicitarias.
+          </p>
+          <h2><br/> $75/hora      ⭐⭐⭐⭐⭐(4.9)</h2>
+        </div>
+        {/* Botón para abrir el componente de agendar cita */}
+        <div className="flex justify-end space-x-4" >
+          <AppointmentButton /> 
+        </div>
       </div>
     </div>
   );
