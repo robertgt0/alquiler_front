ba66c33e84002ef9c2e7e18db0cd23597f90c5dc
Josu-0105 <josueomarmendomamani80@gmail.com>
2025-10-14 00:03:44 -0400

    arreglo de page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import LoginPage from "./teamsys/loginPage";
+
+export default function Page() {
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-gray-100">
+      <LoginPage />
+    </main>
+  );
+}
