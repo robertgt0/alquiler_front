9ef8ad5f997b08c37f1a651696e51c9afdac34bf
Fercho270603 <fbandita12@gmail.com>
2025-10-15 16:33:47 -0400

    feat: Adicion de carrusel inspirador de rama de Ricardo Fermin (Banda Fernando)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,21 @@
 
 import Mapa from "./componentes/mapa/MapaWrapper";
 import Footer from "./componentes/Footer/Footer";
+import CarruselInspirador from "./componentes/CarruselInspirador/CarruselInspirador";
+
 export default function Home() {
   return (
     <main>
       {/* Puedes colocar aquí el contenido de tu página principal */}
       <h1 className="text-4xl font-bold text-center">¡Bienvenido a Servineo!</h1>
       <p className="mt-4 text-lg text-center">Tu plataforma de servicios.</p>
+      <section className="my-5">
+        <CarruselInspirador />
+      </section>
         <section className="my-10">
         <Mapa />
       </section>
+      
       <Footer />
     </main>
     
