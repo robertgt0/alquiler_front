3410df82f4b7c8221f06950cf657ad0d1e369d23
lizkarenarojapinaya <lizkarenarojapinaya@gmail.com>
2025-10-18 00:38:52 -0400

    Refactor scheduling components and update available hours; add new CalendarioPage for better navigation

--- src/app/agenda_proveedor/components/constantes.tsx
+++ src/app/agenda_proveedor/components/constantes.tsx
@@ -25,7 +25,7 @@ export const mesesNombres: string[] = [
 ];
 
 export const horariosDisponibles: HorariosDisponiblesType = {
-  "2025-09-29": [
+  "2025-10-29": [
     { id: 1, horaInicio: "08:00", horaFin: "12:00", costo: 25, moneda: "Bs/Hr." },
     { id: 2, horaInicio: "14:00", horaFin: "18:00", costo: 25, moneda: "Bs/Hr." },
     { id: 3, horaInicio: "02:00", horaFin: "05:00", costo: 35, moneda: "Bs/Hr." }
@@ -36,5 +36,6 @@ export const horariosDisponibles: HorariosDisponiblesType = {
   ],
   "2025-10-24": [
     { id: 1, horaInicio: "10:00", horaFin: "14:00", costo: 30, moneda: "Bs/Hr." }
-  ]
+],
+
 };
