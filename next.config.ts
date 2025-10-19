b0b4074cada89c34a4a8b4c92a53c22914238702
EDSON-CHAMBI-QUISPE <202103868@est.umss.edu>
2025-10-19 07:51:37 -0400

    Actualizar selectivamente globals/layout/next.config/package* con versión de HEAD~2

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // No bloquear el build por ESLint
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // No bloquear el build por errores de tipos
-    ignoreBuildErrors: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
