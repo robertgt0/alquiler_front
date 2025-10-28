a47bf7a8a1d1ed9c6ab6993a77a43d620749c1ef
Yerson <dragon.tuangel123@gmail.com>
2025-10-28 13:27:47 -0400

    PRUEBA2

--- src/app/alquiler/Feature/Componentes/FiltroForm.tsx
+++ src/app/alquiler/Feature/Componentes/FiltroForm.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useFiltros } from "app/alquiler/Feature/Hooks/useFiltro";
 import type { UsuarioResumen } from "app/alquiler/Feature/Types/filtroType";
-//Este es una prueba de comentario para el push
+//Este es una prueba de comentario para el push para verificar si se esta actualizando
 interface FiltrosFormProps {
   onResults?: (usuarios: any[]) => void; // Props pasadas desde paginacion para Ordenamiento y Búsqueda
   sort: string;
