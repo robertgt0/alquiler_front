e78b15834049f4c00e7e3cf70d9ced3de6e6a336
KATeRmZC <katerinmarza@gmail.com>
2025-10-31 18:35:50 -0400

    Resuelto conflicto en page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-<<<<<<< HEAD
-  redirect('/Ordenamiento');
-=======
+
   redirect('/alquiler/paginacion');
->>>>>>> 0d3f19614c8a0153cb80a559e6d552ea9d75a0e2
+
 }
