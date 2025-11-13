22c3a3e64737d6315397e3cf6fd4c86543f60ddd
Rodrigo Alejandro Choque Bautista <rodrigochoquebautista000@gmail.com>
2025-11-13 17:18:46 -0400

    confictos resueltos

--- src/app/UbicacionForm/page.tsx
+++ src/app/UbicacionForm/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-//import UbicacionForm from '../../Features/UbicacionForm/UbicacionForm';
+import UbicacionForm from '../../Features/UbicacionForm/UbicacionForm';
 //import UbicacionForm from '../alquiler/';
 
 /**export default function Page() {
