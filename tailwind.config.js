0fc7b0416c83e909e604eabeda31fba015521422
Rodrigo Alejandro Choque Bautista <rodrigochoquebautista000@gmail.com>
2025-11-12 23:10:28 -0400

    cambio de versiones conflcitos resueltos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
