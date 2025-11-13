27a064bff8753e8747a351ff6cd5d4f5c6a0b2e6
Rodrigo Alejandro Choque Bautista <rodrigochoquebautista000@gmail.com>
2025-11-13 17:23:37 -0400

    correcion de integracion

--- src/app/UbicacionForm/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import UbicacionForm from '../../Features/UbicacionForm/UbicacionForm';
-//import UbicacionForm from '../alquiler/';
-
-/**export default function Page() {
-  return <UbicacionForm />; // <- Con U mayúscula
-}*/
